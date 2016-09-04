c3a057de9a0c1a52caf546cbe3c4d9be363309bd
Heath Stewart <heaths@microsoft.com>
2016-09-03 23:13:39 -0700

    Implement C# sample

---
+++ Setup.Configuration.CS/GlobalSuppressions.cs
@@ -0,0 +1,6 @@
+﻿// <copyright file="GlobalSuppressions.cs" company="Microsoft Corporation">
+// Copyright (C) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt in the project root for license information.
+// </copyright>
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1310:Field names must not contain underscore", Justification = "Win32 error symbol name", Scope = "member", Target = "~F:Program.REGDB_E_CLASSNOTREG")]
--- Setup.Configuration.CS/Program.cs
+++ Setup.Configuration.CS/Program.cs
@@ -3,16 +3,110 @@
 // Licensed under the MIT license. See LICENSE.txt in the project root for license information.
 // </copyright>
 
+using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Setup.Configuration;
+
 /// <summary>
 /// The main program class.
 /// </summary>
 internal class Program
 {
+    private const int REGDB_E_CLASSNOTREG = unchecked((int)0x80040154);
+
     /// <summary>
     /// Program entry point.
     /// </summary>
     /// <param name="args">Command line arguments passed to the program.</param>
-    internal static void Main(string[] args)
+    /// <returns>The process exit code.</returns>
+    internal static int Main(string[] args)
     {
+        try
+        {
+            var query = GetQuery();
+            var query2 = (ISetupConfiguration2)query;
+            var e = query2.EnumAllInstances();
+
+            int fetched;
+            var instances = new ISetupInstance[1];
+            do
+            {
+                e.Next(1, instances, out fetched);
+                if (fetched > 0)
+                {
+                    PrintInstance(instances[0]);
+                }
+            }
+            while (fetched > 0);
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine($"Error 0x{ex.HResult:x8}: {ex.Message}");
+            return ex.HResult;
+        }
+
+        return 0;
     }
+
+    private static ISetupConfiguration GetQuery()
+    {
+        try
+        {
+            // Try to CoCreate the class object.
+            return new SetupConfiguration();
+        }
+        catch (COMException ex) when (ex.HResult == REGDB_E_CLASSNOTREG)
+        {
+            // Try to get the class object using app-local call.
+            ISetupConfiguration query;
+            var result = GetSetupConfiguration(out query, IntPtr.Zero);
+
+            if (result < 0)
+            {
+                throw new COMException("Failed to get query", result);
+            }
+
+            return query;
+        }
+    }
+
+    private static void PrintInstance(ISetupInstance instance)
+    {
+        var instance2 = (ISetupInstance2)instance;
+        var state = instance2.GetState();
+        Console.WriteLine($"InstanceId: {instance2.GetInstanceId()} ({(state == InstanceState.Complete ? "Complete" : "Incomplete")})");
+
+        if ((state & InstanceState.Local) == InstanceState.Local)
+        {
+            Console.WriteLine($"InstallationPath: {instance2.GetInstallationPath()}");
+        }
+
+        if ((state & InstanceState.Registered) == InstanceState.Registered)
+        {
+            Console.WriteLine($"Product: {instance2.GetProduct().GetId()}");
+            Console.WriteLine("Workloads:");
+
+            PrintWorkloads(instance2.GetPackages());
+        }
+
+        Console.WriteLine();
+    }
+
+    private static void PrintWorkloads(ISetupPackageReference[] packages)
+    {
+        var workloads = packages
+            .Where(package => string.Equals(package.GetType(), "Workload", StringComparison.OrdinalIgnoreCase))
+            .OrderBy(package => package.GetId());
+
+        foreach (var workload in workloads)
+        {
+            Console.WriteLine($"    {workload.GetId()}");
+        }
+    }
+
+    [DllImport("Microsoft.VisualStudio.Setup.Configuration.Native.dll", ExactSpelling = true, PreserveSig = true)]
+    private static extern int GetSetupConfiguration(
+        out ISetupConfiguration configuration,
+        IntPtr reserved);
 }
